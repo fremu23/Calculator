e98a76453440723e4dd8311b0b1d2d68573c141f
fremu23 <175006466+fremu23@users.noreply.github.com>
2025-02-18 00:36:45 +1300

    Create the backend framework
    Added the divs for the html and also the basic operation functions.

---
+++ script.js
@@ -0,0 +1,26 @@
+const add = function(num1, num2){
+    return num1 + num2;
+}
+
+const subtract = function(num1, num2){
+    return num1 - num2;
+}
+
+const multiply = function(num1, num2){
+    return num1 * num2;
+}
+
+const divide = function(num1, num2){
+    return num1 / num2;
+}
+
+const operate = function(num1, num2){
+    // if "add" {add}
+    // else if "subtract" {subtract}
+    // else if "multiply" {multiply}
+    // else if "divide" {divide}
+}
+
+// num1 variable
+// operator variable
+// num2 variable
