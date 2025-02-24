2286a41e16c6ded6ca6ec823cc45ec7e7f4f67c9
fremu23 <175006466+fremu23@users.noreply.github.com>
2025-02-25 00:14:05 +1300

    Add document event listener and backspace function
    Created backspace function and put all the Event Listeners in one
    for organisation purposes. Incorporated boolean to ensure that we
    can only type operators one and only after a number is typed.

--- script.js
+++ script.js
@@ -25,13 +25,19 @@ const operate = function(num1, num2){
 // operator variable
 // num2 variable
 
+// Array of Operators
+operators = ["+", "-", "x", "/"];
+
+
+// Reference the Input Box
 const inputBox = document.getElementById("input-box");
+
 
 // Make the calculator button grid
 const numberButtons = document.getElementById("numbers");
 
-const row = 3
-const col = 3
+const row = 3;
+const col = 3;
 
 const containerSize = 12;
 const cellSize = containerSize / row;
@@ -40,16 +46,16 @@ let numberTyped = false;
 
 for (i=0; i < row*col; i++){
     const cell = document.createElement("button");
-    cell.id = i+1
-    cell.style.width = `${cellSize}vw`
+    cell.id = i+1;
+    cell.style.width = `${cellSize}vw`;
     cell.textContent = i+1;
 
     cell.addEventListener("click", () => {
-        inputBox.value += cell.textContent
+        inputBox.value += cell.textContent;
         numberTyped = true;
     });
 
-    numberButtons.appendChild(cell)
+    numberButtons.appendChild(cell);
 }
 
 
@@ -57,24 +63,37 @@ for (i=0; i < row*col; i++){
 // Handling button clicks
 let operator = false;
 
-const addBtn = document.getElementById("add")
-const subBtn = document.getElementById("subtract")
-const mtplyBtn = document.getElementById("multiply")
-const divBtn = document.getElementById("divide")
+document.addEventListener('click', (event) => {
+    const clicked = event.target;
 
-addBtn.addEventListener("click", () => {
-    if (!operator && numberTyped){
-        inputBox.value += addBtn.textContent;
-        operator = true;
+    // Operators to input box
+    if (clicked.matches('#add, #subtract, #multiply, #divide')){
+        if (!operator && numberTyped){
+            inputBox.value += clicked.textContent;
+            operator = true;
+        };
+    }
+
+    // CE Button
+    if (clicked.matches('#clear')){
+        inputBox.value = "";
+        operator = false;
+        numberTyped = false;
+    };
+
+    // Backspace Button
+    if (clicked.matches('#backspace')){
+        const currentVal = inputBox.value;
+        const lastChar = currentVal.slice(-1);
+        const secondLastChar = currentVal.slice(-2, -1);
+        if (operators.includes(lastChar)){
+            operator = false;
+        } else if (secondLastChar == ""){
+            numberTyped = false;
+        }
+        inputBox.value = currentVal.slice(0, -1);
     }
 })
 
-// Clear Entry
-document.getElementById("clear").addEventListener("click", () => {
-    inputBox.value = " ";
-    operator = false;
-    numberTyped = false;
-    console.log("hi");
-});
 
 
